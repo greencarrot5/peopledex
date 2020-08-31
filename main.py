573245cecf42567805dbdd55f82b75b5da3ff715
Greencarrot5 <seppevanammel@gmail.com>
2020-08-31 18:04:01 +0200

    Add webcontent

--- main.py
+++ main.py
@@ -2,7 +2,26 @@
 import os
 path = os.path.dirname(__file__)
 
+# If repl.it doesn't serve web-content, it shuts down
+from flask import Flask
+from threading import Thread
+
+class WebSite(Thread):
+    def __init__(self):
+        super().__init__()
+        self.app = Flask(__name__)
+        @app.route('/')
+        def home():
+            return "Hello world!"
+    
+    def run(self):
+        self.app.run()
+        
+WebSite().run()
+
+# Here starts the bot code
 import discord
+import re
 
 class bot(discord.Client):
     
@@ -12,7 +31,10 @@ class bot(discord.Client):
     
     # When bot receives a message
     async def on_message(self, msg):
-        pass
+        
+        # Check if user pinged bot
+        if re.search("<@749692977483350137>", msg.content):
+            msg.channel.send("Hello, " + str(msg.author) + "!")
 
 peopledex = bot()
 peopledex.run(open(".env").read())
