e6aa9a2e5b95796123d628dd54991508087498a4
greencarrot5 <65950433+greencarrot5@users.noreply.github.com>
2020-12-25 18:03:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,20 @@ WebSite().start()
 # Here starts the bot code
 import discord
 import re
+import urllib.request
+
+def firstImage(q):
+    
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    
+    url = "https://www.google.com/search?q=" + q.replace(" ", "+")
+    headers = {'User-Agent':user_agent} 
+    request = urllib.request.Request(url, headers=headers) #The assembled request
+    response = urllib.request.urlopen(request)
+    data = response.read().decode("utf8")
+    match = re.search("<img src=['\"]([^'\"]*)['\"]", data)
+    src = match.group(1)
+    return src
 
 class bot(discord.Client):
     
@@ -33,8 +47,11 @@ class bot(discord.Client):
     async def on_message(self, msg):
         
         # Check if user pinged bot
-        if re.search("<@749692977483350137>", msg.content):
-            msg.channel.send("Hello, " + str(msg.author) + "!")
+        if msg.content.startsWith("--google "):
+            q = msg.content.replace("--google ", "")
+            embed = discord.Embed(title=q)
+            embed.setImage(firstImage(q))
+            await msg.channel.send(embed)
 
 peopledex = bot()
 peopledex.run(open("env").read())
